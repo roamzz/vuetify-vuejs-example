139c14fcdec6ff36aed61e30dfe6da50f1e80aaa
Irini Pantopoulou <ipantopoulou@draxis.gr>
2020-04-19 20:02:05 +0300

    Login form

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+       <v-card width="400" class="mx-auto mt-5">
+          <v-card-title>
+            <h1>Login</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Username"
+                prepend-icon="mdi-account-circle"
+              />
+              <v-text-field
+               :type="showPassword ? 'text' : 'password'"
+                prepend-icon="mdi-lock"
+                label="Password"
+                @click:append="showPassword = !showPassword"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="success">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="info">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+
+data() {
+    return {
+        showPassword: false
+    }
+},
+};
+</script>
